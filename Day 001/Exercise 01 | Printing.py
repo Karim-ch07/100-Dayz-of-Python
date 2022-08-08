e3a431dde07e712057b75341831420db5f56e06b
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-08 09:46:50 +0200

    Update Exercise 01 | Printing.py

--- Day 001/Exercise 01 | Printing.py
+++ Day 001/Exercise 01 | Printing.py
@@ -2,3 +2,5 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")

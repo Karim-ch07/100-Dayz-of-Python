142a439942e09ebb1ae3789e05245ade5b0d7290
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-09 09:51:16 +0200

    Update Exercise 01 | Data Types.py

--- Day 002/Exercise 01 | Data Types.py
+++ Day 002/Exercise 01 | Data Types.py
@@ -4,4 +4,5 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
+print(Type(two_digit_number))
 print(int(two_digit_number[0]) + int(two_digit_number[1]))

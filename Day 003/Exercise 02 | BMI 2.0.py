17be523125da299c526f9f9dff172bd1cacd9df9
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-10 10:19:51 +0200

    Update Exercise 02 | BMI 2.0.py

--- Day 003/Exercise 02 | BMI 2.0.py
+++ Day 003/Exercise 02 | BMI 2.0.py
@@ -8,16 +8,16 @@ height = float(height)
 weight = float(weight)
 
 BMI = weight/(height ** 2)
-BMI = round(BMI,0)
-BMI = int(BMI)
+BMI = round(BMI)
+#BMI = int(BMI)
 
 if BMI <= 18.5:
     print(f"Your BMI is {BMI}, you are underweight.")
-elif BMI <= 25 and BMI > 18.5:
+elif BMI <= 25:
     print(f"Your BMI is {BMI}, you have a normal weight.")
-elif BMI <= 30 and BMI > 25:
+elif BMI <= 30:
     print(f"Your BMI is {BMI}, you are slightly overweight.")
-elif BMI <= 35 and BMI > 30:
+elif BMI <= 35:
     print(f"Your BMI is {BMI}, you are obese.")
 else:
     print(f"Your BMI is {BMI}, you are clinically obese.")

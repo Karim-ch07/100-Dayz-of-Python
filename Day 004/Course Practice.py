a80fbed641c59b6501a03af3e3ae69fc336e94f4
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-14 19:49:30 +0200

    Update Course Practice.py

--- Day 004/Course Practice.py
+++ Day 004/Course Practice.py
@@ -1,4 +1,5 @@
 import random
+
 a = 0 
 b = 5
 #random.randint(a,b) generates random int values between a and b inclusive
@@ -21,13 +22,14 @@ print(score)
 sample_1 = [0,1,2,2,3]
 sample_2 = ["hey", "Yow", "No"]
 
-#you can access the list's elements using negative indication
+#you can access the list's elements using negative indication as well
 print(sample_1[-1])
 
-#append
+#append > adds an item to the end of the list 
 sample_2.append("Hellanoo!")
 print(sample_2[-1])
 
-#extend
-sample_1.extend("3")
+#extend > adds a list of items to an exsisting list
+sample_1.extend([3, 5])
 print(type(sample_1[-1]))
+print(sample_1[-1])

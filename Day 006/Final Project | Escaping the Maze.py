4446b31a7867abd56079e17eecc48a948ac8bf5d
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-18 17:11:48 +0200

    Update Final Project | Escaping the Maze.py

--- Day 006/Final Project | Escaping the Maze.py
+++ Day 006/Final Project | Escaping the Maze.py
@@ -5,6 +5,11 @@ def turn_right():
     turn_left()
     turn_left()
     
+while front_is_clear():
+    move()
+    
+turn_left()
+
 while not at_goal():
     if right_is_clear():
         turn_right()

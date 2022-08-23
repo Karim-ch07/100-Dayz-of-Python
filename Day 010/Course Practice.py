c546ab02ddcdbebe689f324dbc0e88a25c47b162
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-23 02:58:58 +0200

    Update Course Practice.py

--- Day 010/Course Practice.py
+++ Day 010/Course Practice.py
@@ -1,4 +1,4 @@
-#Functions with outputs
+#Functions with outputs.
 
 def format_name_1(f_name, l_name):
   formated_f_name = f_name.title()
@@ -7,9 +7,11 @@ def format_name_1(f_name, l_name):
 
 print(format_name_1("leo", "MESSI"))
 
-#Functions with multiple return values
+#Functions with multiple return values.
 
 def format_name_2(f_name, l_name):
+  """Take a first and last name and format it
+  to return the title case version of the name."""
   if f_name == "" or l_name == "":
     return "You did not provide a valid input!"
   formated_f_name = f_name.title()
@@ -17,3 +19,8 @@ def format_name_2(f_name, l_name):
   return f"Result : {formated_f_name} {formated_l_name}"
 
 print(format_name_2(input("What is your first name?\t"), input("What is your last name?\t")))
+
+#Already used functions with outputs.
+
+formatted_name = format_name_2(input("What is your first name?\t"), input("What is your last name?\t"))
+length = len(formatted_name)

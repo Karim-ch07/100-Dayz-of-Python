483af5fae33100105e0b4837dd912942e75b24a9
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-23 02:26:30 +0200

    Update Course Practice.py

--- Day 010/Course Practice.py
+++ Day 010/Course Practice.py
@@ -1,1 +1,19 @@
+#Functions with outputs
 
+def format_name_1(f_name, l_name):
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"{formated_f_name} {formated_l_name}"
+
+print(format_name_1("leo", "MESSI"))
+
+#Functions with multiple return values
+
+def format_name_2(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You did not provide a valid input!"
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result : {formated_f_name} {formated_l_name}"
+
+print(format_name_2(input("What is your first name?\t"), input("What is your last name?\t")))

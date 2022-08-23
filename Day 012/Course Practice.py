f0b9fdd6f12d4add4fb380fb642d0d1e9555bd29
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-24 01:54:38 +0200

    Update Course Practice.py

--- Day 012/Course Practice.py
+++ Day 012/Course Practice.py
@@ -23,9 +23,11 @@ drink_potion()
 
 player_health = 10
 
-def drink_potion():
-  potion_strength = 2
-  print(player_health)
+def game():
+  def drink_potion():
+    potion_strength = 2
+    print(player_health)
+  
+  drink_potion()
 
-drink_potion()
 print(player_health)

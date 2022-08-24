4941ead4010a4d609863f96783effa880c5701dc
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-24 02:04:16 +0200

    Update Course Practice.py

--- Day 012/Course Practice.py
+++ Day 012/Course Practice.py
@@ -31,3 +31,38 @@ def game():
   drink_potion()
 
 print(player_health)
+
+#There is no Block Scope
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+if game_level < 5:
+  new = enemies[0]
+
+print(new)
+
+#Modifying Global Scope
+enemies = 1
+
+def increase_enemies():
+  global enemies
+  enemies += 1
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# A better way of doing it is to use return
+enemies = 1
+
+def increase_enemies():
+  print(f"enemies inside function: {enemies}")
+  return enemies + 1
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+#Global Constant
+
+PI = 3.14159
+URL = "https://www.google.com"

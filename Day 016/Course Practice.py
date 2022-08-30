e717c02febfe9bd57ca526c13abe23e41591d8c9
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-30 18:51:43 +0200

    Update Course Practice.py

--- Day 016/Course Practice.py
+++ Day 016/Course Practice.py
@@ -1,1 +1,12 @@
+from turtle import Turtle, Screen
 
+og = Turtle()
+print(og)
+
+og.shape("turtle")
+og.color("coral")
+og.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()

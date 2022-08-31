34c2c47a46b540c23cb870b9b6473c660ecb806c
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-31 09:11:19 +0200

    Update Course Practice.py

--- Day 017/Course Practice.py
+++ Day 017/Course Practice.py
@@ -18,7 +18,19 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0 #default value of the attribute "followers"
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 user_1 = User("007", "agent")
+user_2 = User("007", "agent too")
 
-print(user_1.username)
+user_2.follow(user_1)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)

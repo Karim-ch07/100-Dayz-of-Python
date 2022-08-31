80c048c2e008850ac7b573f07524d1acb1221967
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-31 09:10:54 +0200

    Update Course Practice.py

--- Day 017/Course Practice.py
+++ Day 017/Course Practice.py
@@ -1,12 +1,24 @@
-class User:
-    pass
+# class User:
+#     pass
 
-user_1 = User()
-user_1.id = "007"
-user_1.username = "agent"
+# user_1 = User()
+# user_1.id = "007"
+# user_1.username = "agent"
+
+# print(user_1.username)
+
+# user_2 = User()
+# user_2.id = "007"
+# user_2.username = "agent too"
+
+##################################################################################
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0 #default value of the attribute "followers"
+
+user_1 = User("007", "agent")
 
 print(user_1.username)
-
-user_2 = User()
-user_2.id = "007"
-user_2.username = "agent too"

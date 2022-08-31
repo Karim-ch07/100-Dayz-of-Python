c2622a413f009226dd4b52ae3c494b024c3f6035
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-08-31 09:00:54 +0200

    Update Course Practice.py

--- Day 017/Course Practice.py
+++ Day 017/Course Practice.py
@@ -1,1 +1,12 @@
+class User:
+    pass
 
+user_1 = User()
+user_1.id = "007"
+user_1.username = "agent"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "007"
+user_2.username = "agent too"

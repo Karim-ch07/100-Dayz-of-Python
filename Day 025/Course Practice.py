b0b57667a27abf22543680bae1cd50f43fac7e6a
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 17:47:50 +0100

    Update Course Practice.py

--- Day 025/Course Practice.py
+++ Day 025/Course Practice.py
@@ -1,1 +1,23 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
 
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    day = []
+    condition = []
+    header = False
+    for row in data:
+        if header:
+            day.append(row[0])
+            temperature.append(int(row[1]))
+            condition.append(row[2])
+        header = True
+        print(row)
+
+    print(day)
+    print(temperature)
+    print(condition)

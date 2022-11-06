f8b3ff2b49fce22e9cf107f0d1c764b53e9d3e67
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 02:47:35 +0100

    Update Course Practice.py

--- Day 024/Course Practice.py
+++ Day 024/Course Practice.py
@@ -1,4 +1,8 @@
-file = open("external.txt")
-contents = file.read()
-print(contents)
-file.close()
+# file = open("external.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("external.txt") as file:
+    contents = file.read()
+    print(contents)

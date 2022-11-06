ca2ce11403b1a7dadebef17dd11ebee6634fa49c
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 02:49:21 +0100

    Update Course Practice.py

--- Day 024/Course Practice.py
+++ Day 024/Course Practice.py
@@ -3,6 +3,9 @@
 # print(contents)
 # file.close()
 
-with open("external.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("external.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("external.txt", mode="w") as file:
+    file.write("Next text.")

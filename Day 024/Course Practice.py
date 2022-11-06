a137168d5e89153bb39f8fcfbf4cfae4ebce1cdc
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 02:53:08 +0100

    Update Course Practice.py

--- Day 024/Course Practice.py
+++ Day 024/Course Practice.py
@@ -7,5 +7,11 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("external.txt", mode="w") as file:
+# #the write method will overwrite the existing file with the new string
+# #Also, it is important to remember that files are opened on read only by default. It can not write without setting the right mode
+# with open("external.txt", mode="w") as file:
+#     file.write("Next text.")
+
+#if a file does not exist, it is going to be created
+with open("New.txt", mode="w") as file:
     file.write("Next text.")

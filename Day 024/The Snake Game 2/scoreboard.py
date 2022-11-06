3230b06ecd63fc6fd387d1d14d71611d135fd4fa
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 03:01:55 +0100

    Update scoreboard.py

--- Day 024/The Snake Game 2/scoreboard.py
+++ Day 024/The Snake Game 2/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 250)
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
         self.score = 0
         self.update_scoreboard()
 

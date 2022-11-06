2e53c275eb995294d7d4f06ddc7d2b71351bb3ba
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-06 03:20:30 +0100

    Update Course Practice.py

--- Day 024/Course Practice.py
+++ Day 024/Course Practice.py
@@ -12,6 +12,11 @@
 # with open("external.txt", mode="w") as file:
 #     file.write("Next text.")
 
-#if a file does not exist, it is going to be created
-with open("New.txt", mode="w") as file:
-    file.write("Next text.")
+# #if a file does not exist, it is going to be created
+# with open("New.txt", mode="w") as file:
+#     file.write("Next text.")
+
+#when the file is not in the work directory, we should indicate its absolute path
+with open("/Users/_/OneDrive/Bureau/external.txt") as file:
+    contents = file.read()
+    print(contents)

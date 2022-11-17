814f62bc63ccf7b4b309a175d688a8a68bda6711
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-17 16:08:46 +0100

    Update main.py

--- Day 026/NATO alphabet/main.py
+++ Day 026/NATO alphabet/main.py
@@ -2,6 +2,7 @@ import pandas
 
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
+
 NATO_dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 NATO = {row.letter:row.code for (index,row) in NATO_dataframe.iterrows()}
@@ -9,11 +10,6 @@ NATO = {row.letter:row.code for (index,row) in NATO_dataframe.iterrows()}
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_word = input("Tell me a word!\t").upper()
-user_NATO = []
-
-for user_letter in user_word:
-    for (letter, code) in NATO.items():
-        if user_letter == letter:
-            user_NATO.append(code)
+user_NATO = [NATO[letter] for letter in user_word]
 
 print(user_NATO)

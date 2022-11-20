1a1665adb3e78a49d8a6f0b5769d952b82c6b57e
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-20 21:01:14 +0100

    Update main.py

--- Day 028/Pomodoro/main.py
+++ Day 028/Pomodoro/main.py
@@ -1,42 +1,71 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-def reset_timer():
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_reps():
+    global reps
     reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    reset_reps()
+    canvas.itemconfig(text_timer, text=f"{0:02d}:{0:02d}")
+    label_timer.config(text="TIMER", fg=GREEN)
+    check_marks.set("")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
 
 def start_timer():
     global reps
-    reps = 1
-    while reps != 0:
-        if reps % 2 == 1:
-            count_down(60*WORK_MIN)
-        elif reps % 2 == 0 and reps < 8:
-            count_down(60*SHORT_BREAK_MIN)
-        else:
-            count_down(60*LONG_BREAK_MIN)
-        reps += 1
+    reps += 1
+
+    if reps == 7:
+        reset_timer()
+        # label_timer.config(text="TIMER", fg=GREEN)
+        # reps = 0
+        # allow_start = True
+
+    elif reps % 2 == 1 and reps < 6:
+        # count_down(60*WORK_MIN)
+        label_timer.config(text="WORK", fg=GREEN)
+        count_down(5)
+
+    elif reps % 2 == 0 and reps < 6:
+        # count_down(60*SHORT_BREAK_MIN)
+        label_timer.config(text="SHORT BREAK", fg=PINK)
+        count_down(5)
+
+    else:
+        # count_down(60*LONG_BREAK_MIN)
+        label_timer.config(text="LONG BREAK", fg=RED)
+        count_down(5)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def count_down(count):
+    global timer
 
-    min = math.floor(count/60)
+    min = math.floor(count / 60)
     sec = count % 60
 
-    #Dynamic types
+    # Dynamic types
     # if sec < 10:
     #     sec = f"0{sec}"
     #
@@ -45,7 +74,15 @@ def count_down(count):
     canvas.itemconfig(text_timer, text=f"{min:02d}:{sec:02d}")
 
     if count > 0:
-        window.after(1000, count_down, count-1)
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0 and reps != 0:
+            tmp = check_marks.get()
+            tmp += "✅"
+            check_marks.set(tmp)
+            label_check_marks.config(textvariable=check_marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -60,25 +97,27 @@ canvas.create_image(100, 112, image=img_png)
 text_timer = canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
 canvas.grid(column=1, row=1)
 
-#Count Down
+# Count Down
 
 # count_down(5)
 
-#Label
+# Label
 
 label_timer = Label(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
 label_timer.grid(column=1, row=0)
 
-label_check_marks = Label(text="✅", bg=YELLOW, fg=GREEN)
+check_marks = StringVar()
+check_marks.set("")
+
+label_check_marks = Label(textvariable=check_marks, bg=YELLOW, fg=GREEN)
 label_check_marks.grid(column=1, row=3)
 
-#Button
+# Button
 
 button_start = Button(text="Start", highlightthickness=0, command=start_timer)
 button_start.grid(column=0, row=2)
 
 button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 button_reset.grid(column=2, row=2)
-
 
 window.mainloop()

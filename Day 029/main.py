63d472d828d202c3140e0160c43bef67f9b8cd4b
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-23 17:04:24 +0100

    Add files via upload

--- Day 029/main.py
+++ Day 029/main.py
@@ -4,14 +4,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_date():
+    file = open("data.txt", "a")
+    file.write(f"Website: {input_website}\nEmail/Username:{input_email}\nPasswod:{input_pw}\n\n")
+    file.close()
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Window
 
 window = Tk()
 window.title("Password Generator")
-window.minsize(width=240, height=240)
-window.config(padx=20, pady=20)
+window.config(padx=40, pady=40)
 
 # Canvas
 
@@ -19,15 +23,38 @@ img_png = PhotoImage(file="logo.png")
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 canvas.create_image(100, 100, image=img_png)
-# text_timer = canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
-# canvas.grid(column=1, row=1)
-canvas.pack() # to be modified later on
+canvas.grid(column=1, row=0)
 
 # Label
 
+label_website = Label(text="Website:")
+label_website.grid(column=0, row=1)
+label_website.focus()
+
+label_email = Label(text="Email/Username:")
+label_email.grid(column=0, row=2)
+
+label_pw = Label(text="Password:")
+label_pw.grid(column=0, row=3)
+
 # Input
+
+input_website = Entry(width=59)
+input_website.grid(column=1, row=1, columnspan=2)
+
+input_email = Entry(width=59)
+input_email.grid(column=1, row=2, columnspan=2)
+input_email.insert(0,  string="sample.sample@email.com")
+
+input_pw = Entry(width=34)
+input_pw.grid(column=1, row=3)
 
 # Button
 
+button_generate = Button(text="Generate Password", width=20, highlightthickness=0)
+button_generate.grid(column=2, row=3)
+
+button_add = Button(text="Add", width=50, highlightthickness=0, command=save_date)
+button_add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()

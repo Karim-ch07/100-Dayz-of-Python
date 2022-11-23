6463d6672f90543a6341bba3665115cc1dabd546
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-23 17:52:24 +0100

    Add files via upload

--- Day 029/main.py
+++ Day 029/main.py
@@ -1,12 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_pw():
-    pass
 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_l = [random.choice(letters) for _ in range(nr_letters)]
+    password_s = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_n = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_l + password_s + password_n
+
+    #
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+    #
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+    #
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    #
+    # for char in password_list:
+    #   password += char
+    #
+    # print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 

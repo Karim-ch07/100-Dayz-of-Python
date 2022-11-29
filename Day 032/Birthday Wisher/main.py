951ce5ca0e19bd95b2ca0c5868e036cbceaaa047
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-29 22:52:48 +0100

    Add files via upload

--- Day 032/Birthday Wisher/main.py
+++ Day 032/Birthday Wisher/main.py
@@ -3,14 +3,13 @@ from datetime import *
 from random import *
 from smtplib import *
 
+
 EMAIL_SENDER = "tester.py99@gmail.com"
 APP_PASSWORD = "juslsfmekfbyvpnn"
 PLACEHOLDER = "[NAME]"
 
-
 # 1. Update the birthdays.csv
-listed_data = []
-
+database = {}
 try:
     data = read_csv("birthdays.csv")
 
@@ -18,33 +17,39 @@ except FileNotFoundError:
     print("File Not Found! >> Data CSV.")
 
 else:
-    database = data.to_dict(orient="records")
+    database = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+    print(database)
+
     # 2. Check if today matches a birthday in the birthdays.csv
     now = datetime.now()
+    today = (now.month, now.day)
 
-    for per in database:
-        if now.month == per["month"] and now.day == per["day"]:
-            name = per["name"].title()
-            email = per["email"]
-            # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-            try:
-                path = f"./letter_templates/letter_{randint(1,3)}.txt"
-                with open(path) as letter_file:
-                    letter = letter_file.read()
-                    letter = letter.replace(PLACEHOLDER, f"{name}")
+    for today in database:
 
-            except:
-                print("File Not Found! >> Letter Temps.")
+        per = database[today]
+        name = per["name"].title()
+        email = per["email"]
+        # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the
+        # person's actual name from birthdays.csv
+        try:
+            path = f"./letter_templates/letter_{randint(1, 3)}.txt"
+            with open(path) as letter_file:
+                letter = letter_file.read()
+                letter = letter.replace(PLACEHOLDER, f"{name}")
 
-            # 4. Send the letter generated in step 3 to that person's email address.
-            else:
-                subject = f"Happy Birthday {name}"
-                with SMTP("smtp.gmail.com", port=587) as connect:
-                    connect.starttls()
-                    connect.login(user=EMAIL_SENDER, password=APP_PASSWORD)
-                    connect.sendmail(
-                        from_addr=EMAIL_SENDER,
-                        to_addrs=email,
-                        msg=f"Subject: {subject}\n\n"
-                            f"{letter}"
-                    )
+        except FileNotFoundError:
+            print("File Not Found! >> Letter Temps.")
+
+        # 4. Send the letter generated in step 3 to that person's email address.
+        else:
+            # subject = f"Happy Birthday {name}"
+            # with SMTP("smtp.gmail.com", port=587) as connect:
+            #     connect.starttls()
+            #     connect.login(user=EMAIL_SENDER, password=APP_PASSWORD)
+            #     connect.sendmail(
+            #         from_addr=EMAIL_SENDER,
+            #         to_addrs=email,
+            #         msg=f"Subject: {subject}\n\n"
+            #             f"{letter}"
+            #     )
+            print(f"sent to {email}")

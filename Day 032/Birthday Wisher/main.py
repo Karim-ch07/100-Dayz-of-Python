e83af8d65014a46a3567f177bf007d3c1f445d40
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-29 17:12:39 +0100

    Add files via upload

--- Day 032/Birthday Wisher/main.py
+++ Day 032/Birthday Wisher/main.py
@@ -1,4 +1,3 @@
-##################### Extra Hard Starting Project ######################
 from pandas import *
 from datetime import *
 from random import *
@@ -6,7 +5,6 @@ from smtplib import *
 
 EMAIL_SENDER = "tester.py99@gmail.com"
 APP_PASSWORD = "juslsfmekfbyvpnn"
-
 PLACEHOLDER = "[NAME]"
 
 
@@ -21,25 +19,24 @@ except FileNotFoundError:
 
 else:
     database = data.to_dict(orient="records")
-
-# 2. Check if today matches a birthday in the birthdays.csv
+    # 2. Check if today matches a birthday in the birthdays.csv
     now = datetime.now()
 
     for per in database:
         if now.month == per["month"] and now.day == per["day"]:
             name = per["name"].title()
             email = per["email"]
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+            # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
             try:
                 path = f"./letter_templates/letter_{randint(1,3)}.txt"
-                print(path)
                 with open(path) as letter_file:
                     letter = letter_file.read()
                     letter = letter.replace(PLACEHOLDER, f"{name}")
+
             except:
                 print("File Not Found! >> Letter Temps.")
-# 4. Send the letter generated in step 3 to that person's email address.
 
+            # 4. Send the letter generated in step 3 to that person's email address.
             else:
                 subject = f"Happy Birthday {name}"
                 with SMTP("smtp.gmail.com", port=587) as connect:
@@ -51,8 +48,3 @@ else:
                         msg=f"Subject: {subject}\n\n"
                             f"{letter}"
                     )
-
-
-
-
-

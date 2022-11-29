c91866fafde03fad4010d1027db9d760379e3e10
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-29 15:45:26 +0100

    Add files via upload

--- Day 032/main.py
+++ Day 032/main.py
@@ -1,1 +1,20 @@
+from smtplib import *
 
+email_sender = "tester.py99@gmail.com"
+password = "Li*t0AQGEJ7u("
+app_password = "juslsfmekfbyvpnn"
+
+# email_receiver = "boukhedenna.nedjm@gmail.com"
+email_receiver = "chihanikarim07@gmail.com"
+
+connect = SMTP("smtp.gmail.com", port=587)
+connect.starttls()
+connect.login(user=email_sender, password=app_password)
+connect.sendmail(
+    from_addr=email_sender,
+    to_addrs=email_receiver,
+    msg="Subject: SMTP .py Test Email\n\n"
+        "This is an automated test Email."
+)
+
+connect.close()

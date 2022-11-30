1426f9b251387b4c59ebc20913d22fdb54a4e022
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-11-30 16:12:38 +0100

    Add files via upload

--- Day 033/Kanye Quotes/main.py
+++ Day 033/Kanye Quotes/main.py
@@ -1,10 +1,16 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    data = requests.get(url="https://api.kanye.rest/").json()
+    quote = data["quote"]
+    if len(quote) <= 100:
+        canvas.itemconfig(quote_text, text=quote, font=("Arial", 24, "bold"))
+    elif len(quote) <= 150:
+        canvas.itemconfig(quote_text, text=quote, font=("Arial", 20, "bold"))
+    else:
+        get_quote()
 
 
 window = Tk()
@@ -14,13 +20,11 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 24, "bold"), fill="black")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
-
 
 window.mainloop()

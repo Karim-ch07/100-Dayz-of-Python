0eb111832ef4fe2dd95565440bf5456fbd642e7a
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2022-12-12 16:18:29 +0100

    Add files via upload

--- Day 037/Habit Tracking/main.py
+++ Day 037/Habit Tracking/main.py
@@ -12,6 +12,23 @@ user_params = {
     "notMinor": "yes",
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-print(response.text)
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
 
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph-1",
+    "name": "Programing Graph",
+    "unit": "Lines",
+    "type": "float",
+    "color": "kuro",
+    "timezone": "Poland",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response.text)

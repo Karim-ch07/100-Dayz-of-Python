e91f1e54ea2d8351303519f3d35dcf296261b5e4
Nedjm Boukhedenna <103973697+BNedjm@users.noreply.github.com>
2023-01-08 16:44:13 +0100

    Add files via upload

--- Day 046/WS.py
+++ Day 046/WS.py
@@ -1,8 +1,15 @@
+#
+# import json
+#
+# with open("token.txt", encoding="utf8") as file:
+#     content = json.load(file)
+#     # content = file.read()
+#     access_token = content["access_token"]
+#     print(access_token)
 
-import json
+import spotipy
 
-with open("token.txt", encoding="utf8") as file:
-    content = json.load(file)
-    # content = file.read()
-    access_token = content["access_token"]
-    print(access_token)
+urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
+sp = spotipy.Spotify()
+artist = sp.artist(urn)
+print(artist)
--- Day 046/main.py
+++ Day 046/main.py
@@ -1,10 +1,10 @@
 import spotipy
-from bs4 import BeautifulSoup
 import requests
-from spotipy import *
-from spotipy.oauth2 import SpotifyOAuth
-from pprint import pprint
 import json
+import datetime
+from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
+
 
 CLIENT_ID = ""
 SECRET = ""
@@ -15,6 +15,29 @@ CACHE_PATH = "token.txt"
 URL = "https://www.billboard.com/charts/hot-100"
 
 SPOTIFY_ENDPOINT = "https://api.spotify.com/v1"
+
+date = ""
+date_format = "%Y-%m-%d"
+
+while True:
+    try:
+        date = input("what year you would like to travel to? YYYY-MM-DD\t")
+        dateObject = datetime.datetime.strptime(date, date_format)
+    except ValueError:
+        print("Incorrect data format, should be YYYY-MM-DD")
+    else:
+        break
+
+response = requests.get(f"{URL}/{date}")
+response.raise_for_status()
+spotify_website = response.text
+
+soup = BeautifulSoup(spotify_website, "html.parser")
+titles = [title.getText().strip("\n\t") for title in soup.select(selector="li ul li h3", class_="c-title")]
+artists = [artist.getText().strip("\n\t") for artist in soup.select(selector="li ul li span", class_="c-label") if
+           not artist.getText().strip("\n\t").isdigit() and artist.getText().strip("\n\t") != "-"]
+title_artist_list = dict(zip(titles, artists))
+year = date.split("-")[0]
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -27,24 +50,8 @@ sp = spotipy.Spotify(
     )
 )
 
-# user_id = sp.current_user()["id"]
+user_id = sp.current_user()["id"]
 
-# date = input("what year you would like to travel to? YYYY-MM-DD\t")
-
-# response = requests.get(f"{URL}/{date}")
-response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12")
-response.raise_for_status()
-spotify_website = response.text
-
-# print(spotify_website)
-
-soup = BeautifulSoup(spotify_website, "html.parser")
-
-# titles = soup.select(selector="li ul li h3", id="title-of-a-story")
-# titles = [title.getText() for title in soup.find_all(name="h3", id="title-of-a-story")]
-titles = [title.getText().strip("\n\t") for title in soup.select(selector="li ul li h3", id="title-of-a-story")]
-
-# print(titles)
 with open("token.txt", encoding="utf8") as file:
     content = json.load(file)
     access_token = content["access_token"]
@@ -53,23 +60,66 @@ headers = {
     "Authorization": "Bearer {token}".format(token=access_token)
 }
 
-titles = ["traaaaa", "treksdfd"]
+tracks_uris = []
+
 for title in titles:
     params = {
-        "q": title,
+        "q": f"track:{title} artist:{title_artist_list[title]} year:{year}",
         "type": "track",
-        # "year": "",
     }
-    # try:
-    track_info = requests.get(
+    try:
+        track_info = requests.get(
+            'https://api.spotify.com/v1/search',
+            headers=headers,
+            params=params,
+        ).json()
+        track_uri = track_info["tracks"]["items"][0]["uri"]
+    except IndexError or KeyError:
+        params = {
+            "q": f"track:{title} artist:{title_artist_list[title].split(' ')[0]} year:{year}",
+            "type": "track",
+        }
+        try:
+            track_info = requests.get(
+                'https://api.spotify.com/v1/search',
+                headers=headers,
+                params=params,
+            ).json()
+            track_uri = track_info["tracks"]["items"][0]["uri"]
+        except IndexError or KeyError:
+            continue
+        else:
+            tracks_uris.append(track_uri)
+    else:
+        tracks_uris.append(track_uri)
+
+try:
+    params = {
+        "q": f"{date} Billboard 100",
+        "type": "playlist",
+    }
+    playlist_search = requests.get(
         'https://api.spotify.com/v1/search',
         headers=headers,
         params=params,
+    ).json()
+    playlist_uri = playlist_search["playlists"]["items"][0]["uri"]
+except IndexError or KeyError:
+    playlist = sp.user_playlist_create(
+        user=user_id,
+        name=f"{date} Billboard 100",
+        public=False,
+        collaborative=False,
+        description="NONE",
     )
-    print(track_info.json())
-    # except:
-    #     continue
-    # else:
-    #     pass
-    # finally:
-    #     pass
+    playlist_id = playlist["id"]
+
+    sp.playlist_add_items(
+        playlist_id=playlist_id,
+        items=tracks_uris,
+        position=None,
+    )
+    print("playlist has been created")
+else:
+    sp.playlist_replace_items(playlist_id=playlist_uri, items=tracks_uris)
+    print("playlist has been updated!")
